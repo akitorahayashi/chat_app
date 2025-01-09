5ff13512174f13f30d7fd9b925cf6cdba5c35ded
akitora <hayashi20030703@gmail.com>
2025-01-09 14:43:41 +0900

    Initial commit

---
+++ chat_app/View/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  chat_app
+//
+//  Created by 林 明虎 on 2025/01/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
